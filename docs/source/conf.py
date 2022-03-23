8e0a203cf48e2818073e841d5c63d56bd87cfe3c
varshak3 <varshakrishnakumarr@gmail.com>
2022-03-23 12:40:17 -0500

    Following the tutorial??

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
 templates_path = ['_templates']
 
 # The root document.
-root_doc = 'main.rst'
+root_doc = 'index.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
