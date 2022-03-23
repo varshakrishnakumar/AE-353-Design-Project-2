22e80e9a621874b0d39fc6b7fafd54ea915051b6
varshak3 <varshakrishnakumarr@gmail.com>
2022-03-23 12:01:55 -0500

    Reinstalled Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,52 +1,58 @@
-extensions = ['sphinx.ext.intersphinx']
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-templates_path = ['_templates']
+# -- Path setup --------------------------------------------------------------
 
-source_suffix = '.rst'
-
-
-master_doc = 'index'
-
-# General information about the project.
-project = u'Differential Drive Robot'
-copyright = u'2022, Varsha Krishnakumar'
-
-
-exclude_trees = []
-
-
-pygments_style = 'sphinx'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
-# HTML output 
+# -- Project information -----------------------------------------------------
 
-import sphinx_rtd_theme
+project = 'SegBot'
+copyright = '2022, Varsha Krishnakumar'
+author = 'Varsha Krishnakumar'
 
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_theme_options = {
-   'collapse_navigation': False,
-
-}
+# The full version, including alpha/beta/rc tags
+release = 'March 2022'
 
 
-static_path = ['_static']
+# -- General configuration ---------------------------------------------------
 
-
-
-# Output file - HTML
-basename = 'SegBotDoc'
-
-
-# LaTeX output 
-
-latex_documents = [
-  ('index', 'DjangoKong.tex', u'Django Kong Documentation',
-   u'Eric Holscher', 'manual'),
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
 ]
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
-intersphinx_mapping = {'http://docs.python.org/': None}
-language = 'en'
+# The root document.
+root_doc = 'main.rst'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/source/main.rst.rst
@@ -0,0 +1,26 @@
+.. SegBot documentation master file, created by
+   sphinx-quickstart on Wed Mar 23 11:59:58 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SegBot's documentation!
+==================================
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   :glob:
+
+   install
+   classes
+   methods
+   results
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
